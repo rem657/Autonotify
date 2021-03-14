25014ed9a624eb56da216c530e7035d18cbeed1c
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2021-03-14 14:20:19 -0400

    Update find_3070.py

--- find_3070.py
+++ find_3070.py
@@ -1,3 +1,4 @@
-from main import *
+from src.bot import Bot_I_need_card_plz
+
 if __name__ == '__main__':
     Bot_I_need_card_plz("https://www.newegg.ca/p/pl?d=nvidia+3070&N=100007708&isdeptsrh=1")
